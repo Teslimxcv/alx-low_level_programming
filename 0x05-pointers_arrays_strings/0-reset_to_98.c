28a9a1148e13db6f0a8c64e364a77d0b623915bc
teslimxcv <teslimolakunle19@gmail.com>
2022-04-21 06:59:29 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
-/*
+/**
  * reset_to_98 - reset parameters to 98
- * @n: pointer of the direction at the variable n
- * Return: 0 (success)
+ * @n: pointer
+ * Return: void
  */
 void reset_to_98(int *n)
 {
